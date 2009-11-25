e7c29b4bec82c516e253f010244e52e8fc91fd33
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2009-11-25 16:51:38 +0000

    tburguie: adaptation concernant models (modification fpo)
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1309 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- vigigraph/controllers/rpc.py
+++ vigigraph/controllers/rpc.py
@@ -4,7 +4,7 @@
 from tg import request, expose
 from vigigraph.lib.base import BaseController
 from vigigraph.model import DBSession
-from vigigraph.model import Group, HostGroup, Host
+from vigigraph.model import HostGroup, Host
 
 
 
@@ -19,27 +19,41 @@ class RpcController(BaseController):
     @expose('json')
     def maingroups(self, nocache=None):
         """Render the JSON document for the combobox Main Group"""
-        topgroups = DBSession.query(Group.name, Group.idgroup) \
-                .filter(Group.parent == None) \
-                .order_by(Group.name) \
+        topgroups = DBSession.query(HostGroup.name, HostGroup.idgroup) \
+                .filter(HostGroup.parent == None) \
+                .order_by(HostGroup.name) \
                 .all()
-        return dict(items=[(tpg[0], str(tpg[1])) for tpg in topgroups])
+        if topgroups is not None:
+            return dict(items=[(tpg[0], str(tpg[1])) for tpg in topgroups])
+        return None
     
     @expose('json')
     def hostgroups(self, maingroupid, nocache=None):
         """Render the JSON document for the combobox Other Group"""
-        hostgroups = DBSession.query(Group.name, Group.idgroup)\
-                     .filter(Group.idparent == maingroupid) \
+        hostgroups = DBSession.query(HostGroup.name, HostGroup.idgroup)\
+                     .filter(HostGroup.idparent == maingroupid) \
                      .all()
-        return dict(items=[(hg[0], str(hg[1])) for hg in hostgroups])
+        if hostgroups is not None:
+            return dict(items=[(hg[0], str(hg[1])) for hg in hostgroups])
+        return None
 
     @expose('json')
     def hosts(self, othergroupid, nocache=None):
         """Render the JSON document for the combobox Host Name"""
-        # note actuellement Host.name est la primary_key
-        hosts = DBSession.query(Host.fqhn, Host.name) \
-                .join(HostGroup) \
+        hostgroup = DBSession.query(HostGroup) \
                 .filter(HostGroup.idgroup == othergroupid) \
-                .all()
-        return dict(items=[(h[0], h[1]) for h in hosts])
+                .first()
+        if hostgroup is not None and hostgroup.hosts is not None:
+            return dict(items=[(h.name, h.name) for h in hostgroup.hosts])
+        return None
+
+#    @expose('json')
+#    def graphgroups(self, hostname, nocache=None):
+#        """Render the JSON document for the combobox Graph Group"""
+#        hostgroup = DBSession.query(GraphGroup) \
+#                .filter(HostGroup.idgroup == othergroupid) \
+#                .first()
+#        if hostgroup is not None and hostgroup.hosts is not None:
+#            return dict(items=[(h.name, h.name) for h in hostgroup.hosts])
+#        return None
 
