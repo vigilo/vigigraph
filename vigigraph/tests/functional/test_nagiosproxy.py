c1a039e1c311eeac0e639ce647f3c14088216597
Francis LAHEUGUERE <francis.laheuguere@c-s.fr>
2010-02-02 13:31:04 +0000

    Ajout de tests (méthodes utilisées sous vigigraph)
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1826 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ vigigraph/tests/functional/test_nagiosproxy.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 02 feb. 2010
+@author: flaheugu
+
+Tests Nagios Proxy
+'''
+
+import unittest
+
+from vigigraph.controllers.nagiosproxy import NagiosProxy
+
+from vigilo.common.conf import settings
+
+
+class NagiosProxy_without_nagios(NagiosProxy):
+    """ Classe de substitution de NagiosProxy pour effectuer les tests sans nagios"""
+    def __init__(self, content, *args, **kwargs):
+        super(NagiosProxy_without_nagios, self).__init__(*args, **kwargs)
+        self.content = content
+
+    def _retrieve_content(self, *args, **kwargs):
+        return self.content
+
+
+class TestNagiosProxy(unittest.TestCase):
+    """ Test Gestion Valeur Metrologie """  
+
+    def __init__(self, *args, **kwargs):
+        super(TestNagiosProxy, self).__init__(*args, **kwargs)
+        #self.url = settings.get('RRD_URL') -> ne marche pas
+        self.url = 'http://localhost'
+
+    def test_subPage(self):
+        '''fonction vérification subPage'''
+        result = None
+
+        host = 'par.linux0'
+
+        content = '''<html><head>
+        </head></html>
+        '''
+
+        url = self.url
+        if url is not None:
+            nagiosproxy = NagiosProxy_without_nagios(content, url)
+            result = nagiosproxy.get_status(host)
+
+        assert(result != None)
+    
+    def test_servicePage(self):
+        '''fonction vérification servicePage'''
+        result = None
+
+        host = 'par.linux0'
+        service = 'Load'
+
+        content = '''<html><head>
+        </head></html>
+        '''
+
+        url = self.url
+        if url is not None:
+            nagiosproxy = NagiosProxy_without_nagios(content, url)
+            result = nagiosproxy.get_extinfo(host, service)
+
+        assert(result != None)
+
+
+if __name__ == "__main__": 
+    unittest.main()
