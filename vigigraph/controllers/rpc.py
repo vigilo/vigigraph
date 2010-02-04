abb1d154be2599c314edc8d5b46ac02fffea25a5
Francis LAHEUGUERE <francis.laheuguere@c-s.fr>
2010-02-04 16:57:57 +0000

    Impression graphe - ajout periode
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1858 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- vigigraph/controllers/rpc.py
+++ vigigraph/controllers/rpc.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """RPC controller for the combobox of vigigraph"""
 
-from tg import expose, response
+from tg import expose, response, request
 
 from vigigraph.lib.base import BaseController
 
@@ -29,6 +29,9 @@ import random
 import urllib
 import urllib2
 import csv
+
+from time import gmtime, strftime
+
 
 __all__ = ['RpcController']
 
@@ -349,6 +352,7 @@ class RpcController(BaseController):
     def graphsList(self, nocache=None, **kwargs):
         '''graphsList'''
         graphslist = []
+        format = "%d-%m-%Y %H:%M"
         for key in kwargs:
             # titre
             title = "Inconnu"
@@ -360,15 +364,21 @@ class RpcController(BaseController):
                 for arg in largs:
                     larg = arg.split("=")
                     if len(larg) == 2:
-                        if larg[0] == "graphtemplate":
-                            graph = larg[1]
-                        elif larg[0] == "server":
+                        if larg[0] == "server":
                             server = larg[1]
+                        elif larg[0] == "graphtemplate":
+                            graph = larg[1]
+                        elif larg[0] == "start":
+                            start = larg[1]
+                        elif larg[0] == "duration":
+                            duration = larg[1]
             if graph != "" or server != "":
                 title = "'%s' Graph for host %s" % \
                   (urllib.unquote_plus(graph), server)
             graph = {}
             graph['title'] = title
+            graph['sts'] = _(strftime(format, gmtime(int(start))))
+            graph['ets'] = _(strftime(format, gmtime(int(start) + int(duration))))
             graph['src'] = urllib2.unquote(kwargs[key])
             graphslist.append(graph)
         return dict(graphslist=graphslist)
@@ -525,3 +535,62 @@ class RpcController(BaseController):
 
         if b_export == False:
             return 'KO'
+
+    @expose('')
+    def fullHostPage(self, host, start=None, duration=86400):
+        """
+        """
+        presels=[
+            {"caption" : "Last 12h", "duration" : 43200},
+            {"caption" : "Last 24h", "duration" : 86400},
+            {"caption" : "Last 2d",  "duration" : 192800},
+            {"caption" : "Last 7d",  "duration" : 604800},
+            {"caption" : "Last 14d", "duration" : 1209600},
+            {"caption" : "Last 3m", "duration" : 86400*31*3},
+            {"caption" : "Last 6m", "duration" : 86400*183},
+            {"caption" : "Last year", "duration" : 86400*365},
+        ]
+        if start is None:
+            start = int(time.time()) - int(duration)
+        request.content_type = "text/html"
+        ret = """
+        <html>
+            <head>
+                <title>Graph Page for %s</title>
+                <meta http-equiv="Refresh" content="300">
+                <meta http-equiv="Pragma" content="no-cache">
+                <meta http-equiv="Cache-Control" content="no-cache">
+                <link rel="search" type="application/opensearchdescription+xml" title="Supervision search" href="SupNavigator.py/getOpenSearch">
+                <style type="text/css"><!--@import url(../styles/style.css); --></style>
+            </head>
+            <body class='main'>
+            <div class="header"><style type="text/css"><!--@import url(../styles/tmpl.default.css); --></style>
+                <table class="header_table">
+                    <tr>
+                        <td class="logo">
+                            <a href="/proto4">
+                            <img class="logo" src="../styles/red15-logo-vigilo.png" alt="Vigilo"/>
+                            </a> v2.0
+                        </td>
+                    </tr>
+                </table>
+            </div>
+                <h2>Graph Page for %s</h2>
+        """%(host,host)
+        ret += """[<a href="supPage?host=%s">%s's monitoring page</a>]"""%(host, host)
+        for h in presels:
+            ret += """[<a href="fullHostPage?host=%s&duration=%s">%s</a>]"""%(host,h["duration"],h["caption"])
+        ret += "<br />"
+        '''
+        l = navconf.hosts[host]['graphs'].keys()
+        l.sort()
+        for i in l:
+#            ret += "<h3>%s</h3>\n"%i
+            v1 = navconf.hosts[host]['graphs'][i]
+            v1.sort()
+            for j in v1:
+                ret += """<a href="singleGraph?host=%s&graph=%s&start=%s&duration=%s"><img border="0" src="getImage?host=%s&graph=%s&start=%s&duration=%s&details=0" title="%s"/></a>\n"""%(host,urllib.quote_plus(j),start,duration,host,urllib.quote_plus(j),start,duration,j)
+        '''
+        ret += "</body></html>"
+        #request.write(ret)
+        return ret
