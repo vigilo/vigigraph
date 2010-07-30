301d508e333db8d3a967f2bd2fe47610c7e1ffe5
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-07-30 09:57:40 +0000

    Modification des applications pour qu'elles utilisent un fichier de configuration séparé pour l'authentification (who.ini).
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@3991 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- vigigraph/config/app_cfg.py
+++ vigigraph/config/app_cfg.py
@@ -8,10 +8,10 @@ This file complements development/deployment.ini.
 Please note that **all the argument values are strings**. If you want to
 convert them into boolean, for example, you should use the
 :func:`paste.deploy.converters.asbool` function, as in::
-    
+
     from paste.deploy.converters import asbool
     setting = asbool(global_conf.get('the_setting'))
- 
+
 """
 
 import vigigraph
@@ -20,24 +20,3 @@ from vigigraph.lib import app_globals, helpers
 
 base_config = VigiloAppConfig('vigigraph')
 base_config.package = vigigraph
-
-# Configure the authentication backend
-base_config.auth_backend = 'sqlalchemy'
-
-# override this if you would like to provide a different who plugin for
-# managing login and logout of your application
-base_config.sa_auth.form_plugin = None
-
-# You may optionally define a page where you want users to be redirected to
-# on login:
-base_config.sa_auth.post_login_url = '/post_login'
-
-# You may optionally define a page where you want users to be redirected to
-# on logout:
-base_config.sa_auth.post_logout_url = '/post_logout'
-
-
-##################################
-# Settings specific to Vigigraph #
-##################################
-
