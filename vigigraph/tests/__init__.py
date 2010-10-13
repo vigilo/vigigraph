07694f25f20c8af413f315d2f59a1844e3df9ebf
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-10-13 16:58:18 +0000

    Suppression référence à url_for (pas utilisé/utilise dans/par les tests).
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4938 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- vigigraph/tests/__init__.py
+++ vigigraph/tests/__init__.py
@@ -8,12 +8,11 @@ import unittest
 from tg import config
 from paste.deploy import loadapp
 from paste.script.appinstall import SetupCommand
-from routes import url_for
 from webtest import TestApp
 
 from vigilo.models.session import metadata, DBSession
 
-__all__ = ['setup_db', 'teardown_db', 'TestController', 'url_for']
+__all__ = ['setup_db', 'teardown_db', 'TestController']
 
 def setup_db():
     """Method used to build a database"""
@@ -30,22 +29,22 @@ def teardown_db():
 class TestController(unittest.TestCase):
     """
     Base functional test case for the controllers.
-    
-    The vigigraph application instance (``self.app``) set up in this test 
+
+    The vigigraph application instance (``self.app``) set up in this test
     case (and descendants) has authentication disabled, so that developers can
     test the protected areas independently of the :mod:`repoze.who` plugins
     used initially. This way, authentication can be tested once and separately.
-    
+
     Check vigigraph.tests.functional.test_authentication for the repoze.who
     integration tests.
-    
+
     This is the officially supported way to test protected areas with
     repoze.who-testutil (http://code.gustavonarea.net/repoze.who-testutil/).
-    
+
     """
-    
+
     application_under_test = 'main_without_authn'
-    
+
     def setUp(self):
         """Method called by nose before running each test"""
         # Loading the application:
@@ -57,10 +56,9 @@ class TestController(unittest.TestCase):
         test_file = path.join(conf_dir, 'test.ini')
         cmd = SetupCommand('setup-app')
         cmd.run([test_file])
-    
+
     def tearDown(self):
         """Method called by nose after running each test"""
         # Cleaning up the database:
         teardown_db()
         del self.app
-
