f424679c43d0f39d58bbe4f24cf5666b69aa04a9
Francois POIROTTE <francois.poirotte@c-s.fr>
2011-01-24 14:51:25 +0000

    Modification pour pylint : évite un avertissement sur la redéfinition de la fonction globale.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@5904 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- vigigraph/websetup.py
+++ vigigraph/websetup.py
@@ -5,11 +5,11 @@ __all__ = ['setup_app', 'populate_db']
 
 def setup_app(command, conf, variables):
     """Place any commands to setup vigigraph here"""
-    from vigilo.turbogears import populate_db
+    from vigilo.turbogears import populate_db as tg_pop_db
     from vigigraph.config.environment import load_environment
 
     load_environment(conf.global_conf, conf.local_conf)
-    populate_db()
+    tg_pop_db()
 
 def populate_db(bind):
     from vigilo.models.session import DBSession
