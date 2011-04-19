90a1f4abfe75d4d53f5669d0c9f0ca2ba321964f
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-04-19 18:00:30 +0000

    Ajout des copyrights manquants
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@6846 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- vigigraph/public/js/toolbar.js
+++ vigigraph/public/js/toolbar.js
@@ -1,3 +1,6 @@
+// Copyright (C) 2006-2011 CS-SI
+// License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
 Jx.Button.SelectorFlyout = new Class({
     Extends: Jx.Button.Flyout,
 
--- vigigraph/tests/functional/test_graph_selection_form.py
+++ vigigraph/tests/functional/test_graph_selection_form.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2006-2011 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
 """
 Suite de tests de l'arbre de sélection des graphes et groupes de graphes.
 """
