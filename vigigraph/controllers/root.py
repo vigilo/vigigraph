bba06e7957f43e6a31d2766b021864fa103722d9
Francois POIROTTE <francois.poirotte@c-s.fr>
2011-11-04 15:05:52 +0100

    Activation du cache des permissions (#577)
    Modifie l'ordre des middlewares WSGI afin que les couches apportées par
    Beaker (gestion du cache et gestion des sessions) soient disponibles
    depuis les couches d'authentification/autorisations (repoze.who et
    repoze.what).
    
    Ceci permet d'activer réellement le cache des permissions au niveau du
    plugin Kerberos (auparavant, la fonctionnalité était présent, mais
    inutilisable car Beaker ne se trouvait pas dans la pile de couches).
    
    Ce changeset remplace également le plugin d'authentification Kerberos
    pour supprimer les méthodes responsables de l'identification et de
    l'authentification de l'utilisateur par une unique méthode d'ajout de
    méta-données (mdprovider).
    De fait, le plugin sera correctement appelé avec le nouveau code pour la
    pile d'authentification (afin de créer l'utilisateur dans Vigilo
    correspondant à l'utilisateur externe).
    Le plugin a été renommé (nom plus explicite/réaliste) et déplacé à cet
    endroit : `vigilo.turbogears.repoze.plugins.mdldapsync`.
    Ceci introduit une dépendance sur python-ldap dans vigilo.turbogears.
    
    Ce changement centralise tout ce qui concerne repoze.wh(o|at) dans
    vigilo.turbogears.repoze.
    Le classifier a été modifié pour identifier spécifiquement les requêtes
    authentifiée par un mécanisme externe (ex: Kerberos).
    Les plugins ont vu leur nom changer radicalement et se trouvent
    désormais chacun dans un fichier séparé (sqlauth, mduser, mdgroups).
    
    Un nouveau module (d'identification) pour repoze.who a été ajouté dans
    `vigilo.turbogears.repoze.plugins.externalid`. Ce module permet de
    pré-authentifier un utilisateur à partir du contenu d'une session
    Beaker. Il sera utilisé avec `mdldapsync` qui mémorise déjà l'identité
    d'un utilisateur authentifié via un mécanisme externe dans la session
    Beaker.
    
    Les fichiers .ini des différentes IHM ont été mis à jour en conséquence.
    
    Les méthodes liées à l'authentification dans les contrôleurs !TurboGears
    ont été mises en commun dans `vigilo.turbogears.controllers.auth`.
    
    Refs: #577.
    Change-Id: Ia2d600622fd623e1a4474a4ea24fef3b3b503c12
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/377
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Aurelien BOMPARD <aurelien.bompard@c-s.fr>

--- vigigraph/controllers/root.py
+++ vigigraph/controllers/root.py
@@ -12,7 +12,7 @@ from repoze.what.predicates import Any, All, not_anonymous, \
                                     has_permission, in_group
 from pkg_resources import resource_filename
 
-from vigilo.turbogears.controllers import BaseController
+from vigilo.turbogears.controllers.auth import AuthController
 from vigilo.turbogears.controllers.error import ErrorController
 from vigilo.turbogears.controllers.proxy import ProxyController
 from vigilo.turbogears.controllers.api.root import ApiRootController
@@ -24,7 +24,7 @@ __all__ = ['RootController']
 LOGGER = logging.getLogger(__name__)
 
 # pylint: disable-msg=R0201
-class RootController(BaseController):
+class RootController(AuthController):
     """
     The root controller for the vigigraph application.
     """
@@ -99,43 +99,3 @@ class RootController(BaseController):
             fhandle.close()
 
         return translations
-
-    @expose('login.html')
-    def login(self, came_from='/'):
-        """Start the user login."""
-        login_counter = request.environ['repoze.who.logins']
-        if login_counter > 0:
-            flash(_('Wrong credentials'), 'warning')
-        return dict(page='login', login_counter=str(login_counter),
-                    came_from=came_from)
-
-    @expose()
-    def post_login(self, came_from='/'):
-        """
-        Redirect the user to the initially requested page on successful
-        authentication or redirect her back to the login page if login failed.
-
-        """
-        if not request.identity:
-            login_counter = request.environ['repoze.who.logins'] + 1
-            redirect('/login', came_from=came_from, __logins=login_counter)
-        userid = request.identity['repoze.who.userid']
-        LOGGER.info(_('"%(username)s" logged in (from %(IP)s)') % {
-                'username': userid,
-                'IP': request.remote_addr,
-            })
-        flash(_('Welcome back, %s!') % userid)
-        redirect(came_from)
-
-    @expose()
-    def post_logout(self, came_from='/'):
-        """
-        Redirect the user to the initially requested page on logout and say
-        goodbye as well.
-
-        """
-        LOGGER.info(_('Some user logged out (from %(IP)s)') % {
-                'IP': request.remote_addr,
-            })
-        flash(_('We hope to see you soon!'))
-        redirect(came_from)
