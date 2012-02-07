a7525938ed21722b04561676204413b26b94d6b5
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-02-07 16:17:22 +0100

    Bump des versions
    On les met toutes à la même version, sans chercher à détecter ce qui a
    changé (par sécurité).
    
    BuildBot: no-build
    Change-Id: Ibf29fbc64cd76c0a3a013c08b180c00335c8d28b

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = [
 
 setup(
     name='vigilo-vigigraph',
-    version='2.0.11',
+    version='2.0.13',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org',
