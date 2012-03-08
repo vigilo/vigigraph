16eb2b85164a1276fb999f430f7561cf3ab13be0
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-03-08 12:26:19 +0100

    Bump des versions
    Change-Id: I41c67f0ba280245bbdcfbf1e7e28dfc7a5fcdb97
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = [
 
 setup(
     name='vigilo-vigigraph',
-    version='2.0.15',
+    version='2.1.0',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org',
