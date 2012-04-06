7c76ae22488119775dda914ec6697bb315fc1cbb
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-04-06 11:42:48 +0200

    Ajout et mises à jour de copyrights, complète le [sha:3c99844]
    Change-Id: I47c53310f710006097927c666387207c1f4125aa
    Fixup: 3c99844c7630b66e3f267cc08d169a5c91b73572
    BuildBot: no-build

--- vigigraph/lib/__init__.py
+++ vigigraph/lib/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2011-2012 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
 """
 Code annexe de VigiGraph, qui ne génère pas de contenu visible
 par les utilisateurs.
