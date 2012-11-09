0158c6f0150ef642bb77f9c42d23264e850cc8be
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-11-09 11:17:40 +0100

    Bump des versions pour sortie de Vigilo v3.0.
    Change-Id: Ic408886aecd69097c715064854e1e0c420a71080
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/962
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = [
 
 setup(
     name='vigilo-vigigraph',
-    version='2.1.2',
+    version='3.0.0',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org',
