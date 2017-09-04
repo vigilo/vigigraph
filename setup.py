55a01014c351d2beac3a5081bf0631d5643c6bd5
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-09-04 16:09:29 +0200

    Bump version 4.1.0rc3
    Change-Id: I6a5f29c6ce4b2f1207d47ac4c1649e2c045a703c
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ tests_require = [
 
 setup(
     name='vigilo-vigigraph',
-    version='4.1.0rc2',
+    version='4.1.0rc3',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     url='http://www.vigilo-nms.com',
