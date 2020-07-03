cf9c9939fb6f17c19ec1f77cc11a54fd32e3c632
Francois POIROTTE <francois.poirotte@c-s.fr>
2020-07-03 14:08:25 +0200

    Bump v5.2.0b2
    Change-Id: I44eda5e8a05730f8063ffefd7efc08432b316cdf

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ tests_require = [
 
 setup(
     name='vigilo-vigigraph',
-    version='5.2.0b1',
+    version='5.2.0b2',
     author='Vigilo Team',
     author_email='contact.vigilo@csgroup.eu',
     url='https://www.vigilo-nms.com',
